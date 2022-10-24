3c023c79f7e9b117f644b14094b12fef24822f00
ebauschatz <eaenglish113@gmail.com>
2022-10-24 10:58:08 -0500

    feat: adding dislay of all songs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MusicTable from './Components/MusicTable/MusicTable';
+
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs() {
+    let response = await axios.get("http://127.0.0.1:8000/api/music/");
+    setSongs(response.data);
+  }
+
   return (
-    <div>TEST</div>
+    <div>
+      <MusicTable songs={songs} />
+    </div>
   );
 }
 
---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,36 @@
+import './MusicTable.css'
+
+const MusicTable = (props) => {
+    return (
+        <table className="table-style">
+            <thead>
+                <tr>
+                    <th className="song-element">ID</th>
+                    <th className="song-element">Title</th>
+                    <th className="song-element">Artist</th>
+                    <th className="song-element">Album</th>
+                    <th className="song-element">Release Date</th>
+                    <th className="song-element">Genre</th>
+                    <th className="song-element">Likes</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.songs.map((song) => {
+                    return (
+                        <tr key={song.id}>
+                            <td className="song-element">{song.id}</td>
+                            <td className="song-element">{song.title}</td>
+                            <td className="song-element">{song.artist}</td>
+                            <td className="song-element">{song.album}</td>
+                            <td className="song-element">{song.release_date}</td>
+                            <td className="song-element">{song.genre}</td>
+                            <td className="song-element">{song.likes}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    );
+}
+ 
+export default MusicTable;
