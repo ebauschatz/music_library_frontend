06ba602580c1806a1c5aa0508091cad452b82461
ebauschatz <eaenglish113@gmail.com>
2022-10-26 16:59:02 -0500

    feat: adding header and some colors

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-bar">
+            <h2>MusicLibrary</h2>
+        </div>
+    );
+}
+ 
+export default Header;
